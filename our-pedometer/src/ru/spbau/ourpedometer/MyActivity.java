7f20f265daf11207ef69b00ced18162b81a4b6f3
fofanova.mn <fofanova.mn@gmail.com>
2012-04-19 11:57:34 +0000

    initial commit

--- our-pedometer/src/ru/spbau/ourpedometer/MyActivity.java
+++ our-pedometer/src/ru/spbau/ourpedometer/MyActivity.java
@@ -7,11 +7,15 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.example.R;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,15 +33,28 @@ public class MyActivity extends Activity implements SensorEventListener
     List<List<Float>> values;
     boolean isRecording = false;
 
+    BufferedWriter writer;
+
 
     View.OnClickListener startListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             isRecording = !isRecording;
             if(isRecording) {
-                values.get(SensorManager.DATA_X).clear();
-                values.get(SensorManager.DATA_Y).clear();
-                values.get(SensorManager.DATA_Z).clear();
+                values.clear();
+
+                try{
+
+                    File file = new File(Environment.getExternalStorageDirectory() + File.separator + "test.txt");
+                    writer = new BufferedWriter(new FileWriter(file));
+
+                    writer.newLine();
+                    writer.write("Start!");
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     };
@@ -61,6 +78,7 @@ public class MyActivity extends Activity implements SensorEventListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+
         values = new ArrayList<List<Float>>();
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
@@ -69,10 +87,15 @@ public class MyActivity extends Activity implements SensorEventListener
                 switch(sensor.getType()) {
                     case Sensor.TYPE_ACCELEROMETER:
                         if(accelerometerSensor == null) accelerometerSensor = sensor;
+                        break;
+                }
+                if(accelerometerSensor != null){
                     break;
                 }
             }
         }
+
+
 
         valueX = (TextView)findViewById(R.id.value_x);
         valueY = (TextView)findViewById(R.id.value_y);
@@ -85,17 +108,38 @@ public class MyActivity extends Activity implements SensorEventListener
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        if(!isRecording) return;
         final List<Float> lValues = new ArrayList<Float>();
+        try {
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error during collecting statistics!");
+            e.printStackTrace();
+        }
         for(float value: sensorEvent.values) {
             lValues.add(value);
+            try {
+                writer.write(String.valueOf(value) + " ");
+            } catch (IOException e) {
+                System.err.println("Error during collecting statistics!");
+                e.printStackTrace();
+            }
         }
+
+        try {
+            writer.flush();
+        } catch (IOException e) {
+            System.err.println("Error during saving statistics!");
+            e.printStackTrace();
+        }
+
         switch(sensorEvent.sensor.getType()) {
             case Sensor.TYPE_ACCELEROMETER:
                 valueX.setText(String.valueOf(sensorEvent.values[SensorManager.DATA_X]));
                 valueY.setText(String.valueOf(sensorEvent.values[SensorManager.DATA_Y]));
                 valueZ.setText(String.valueOf(sensorEvent.values[SensorManager.DATA_Z]));
                 values.add(lValues);
-            break;
+                break;
         }
     }
 
