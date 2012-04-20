8c48bea709db4820cb76e06eee908e0c0d5c1971
fofanova.mn@gmail.com <fofanova.mn@gmail.com@86b15b9a-38e2-1469-628e-0faded9aee6c>
2012-04-20 21:38:40 +0000

    Normal commit&

--- trunk/our-pedometer/src/ru/spbau/ourpedometer/MainWidget.java
+++ trunk/our-pedometer/src/ru/spbau/ourpedometer/MainWidget.java
@@ -1,11 +1,13 @@
 package ru.spbau.ourpedometer;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.*;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -32,7 +34,7 @@ public class MainWidget extends AppWidgetProvider{
 
     public static void setAimSteps(int aimSteps) {
         MainWidget.aimSteps = aimSteps;
-        views.setProgressBar(R.id.progress, maxProgress, ++aimSteps, false);
+        views.setProgressBar(R.id.process, maxProgress, ++aimSteps, false);
         wm.updateAppWidget(ids, views);
     }
     public void onUpdate(final Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -67,8 +69,8 @@ public class MainWidget extends AppWidgetProvider{
                 int steps = 0;
                 if(!binded){
                     RemoteServiceConnection mConnection = new RemoteServiceConnection();
-                    binded = context.bindService(new Intent(PedometerRemoteInterface.class.getName()),
-                            mConnection, Context.BIND_AUTO_CREATE);
+                   /* binded = context.bindService(new Intent(PedometerRemoteInterface.class.getName()),
+                            mConnection, Context.BIND_AUTO_CREATE); */
                 } else {
                     try {
                         steps = aService.getSteps();
@@ -80,7 +82,7 @@ public class MainWidget extends AppWidgetProvider{
                     }
                 }
 
-                views.setProgressBar(R.id.progress, aimSteps, steps, false);
+                views.setProgressBar(R.id.process, aimSteps, steps, false);
                 //views.setTextViewText(R.id.hmCounter, "Speed: " + hoursLeft--);
                 wm.updateAppWidget(ids, views);
             }
@@ -122,10 +124,10 @@ public class MainWidget extends AppWidgetProvider{
                 PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, 0);
                 NotificationManager notificationManager =
                         (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-                Notification noty = new Notification(R.drawable.icon, "Button 1 clicked",
+              /*  Notification noty = new Notification(R.drawable.icon, "Button 1 clicked",
                         System.currentTimeMillis());
                 noty.setLatestEventInfo(context, "Notice", msg, contentIntent);
-                notificationManager.notify(1, noty);
+                notificationManager.notify(1, noty);*/
             }
             super.onReceive(context, intent);
         }
