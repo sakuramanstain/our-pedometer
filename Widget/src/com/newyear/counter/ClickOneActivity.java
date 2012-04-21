4cbc6c105326be07cb16bd0596f3d9d0df9f6349
fofanova.mn <fofanova.mn@gmail.com>
2012-04-21 07:04:26 +0000

    [Maria]Mega Super Widget!!!

---
+++ Widget/src/com/newyear/counter/ClickOneActivity.java
@@ -0,0 +1,48 @@
+package com.newyear.counter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ClickOneActivity extends Activity {
+    Button startButton;
+
+    View.OnClickListener startListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText editText = ((EditText) findViewById(R.id.txtFieldId));
+            Editable text = null;
+            if(editText != null) {
+                text = editText.getText();
+            }
+            if(text != null) {
+                MainWidget.setProgress(Integer.parseInt(text.toString()));
+            }
+            Toast.makeText(getApplicationContext(), "Progress was set.",
+                    Toast.LENGTH_SHORT).show();
+            moveTaskToBack(true);
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configure);
+
+        Toast.makeText(getApplicationContext(), "We are in ClickOneActivity",
+                Toast.LENGTH_SHORT).show();
+
+
+        startButton = (Button) findViewById(R.id.button_set_progress);
+        startButton.setOnClickListener(startListener);
+    }
+
+
+
+
+}
