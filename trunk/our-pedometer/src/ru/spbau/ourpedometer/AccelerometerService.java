bd4b8cd63a392d8f2cfb769648ee9775d23bb046
fofanova.mn@gmail.com <fofanova.mn@gmail.com@86b15b9a-38e2-1469-628e-0faded9aee6c>
2012-04-23 06:47:09 +0000

    [Maria] Added setting of start time

--- trunk/our-pedometer/src/ru/spbau/ourpedometer/AccelerometerService.java
+++ trunk/our-pedometer/src/ru/spbau/ourpedometer/AccelerometerService.java
@@ -11,19 +11,19 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.sql.Time;
+import java.util.*;
 
 public class AccelerometerService extends Service implements SensorEventListener {
 
     public static final int FIRST_RUN = 2000;
-    public static final int INTERVAL= 5000;
+    public static final int INTERVAL = 5000;
     public static final String STEPS_BROADCAST_ACTION = "ru.spbau.ourpedometer.ACCELEROMETER_BROADCAST";
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
     private Timer timer;
+
+    private static Time timeSinceStart;
 
     @Override
     public void onCreate() {
@@ -41,7 +41,7 @@ public class AccelerometerService extends Service implements SensorEventListener
     @Override
     public void onDestroy() {
         timer.cancel();
-        sensorManager.unregisterListener(this , accelerometerSensor);
+        sensorManager.unregisterListener(this, accelerometerSensor);
     }
 
     private void startService() {
@@ -55,7 +55,7 @@ public class AccelerometerService extends Service implements SensorEventListener
                         break;
                 }
                 if (accelerometerSensor != null) {
-                    sensorManager.registerListener(this, accelerometerSensor,SensorManager.SENSOR_DELAY_GAME );
+                    sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
                     break;
                 }
             }
@@ -63,22 +63,23 @@ public class AccelerometerService extends Service implements SensorEventListener
             timer.schedule(new TimerTask() {
                 @Override
                 public void run() {
-                    try{
+                    try {
+
                         Intent intent = new Intent(STEPS_BROADCAST_ACTION);
                         intent.putExtra("steps",
-                                StatisticsManager.getCalculator().steps(new Date(0), new Date()));
+                                StatisticsManager.getCalculator().steps(startDate(), new Date()));
                         sendBroadcast(intent);
-                    } catch (Exception ex){
+                    } catch (Exception ex) {
                         Log.d(AccelerometerService.class.getName(), ex.getMessage());
                     }
                 }
-            }, FIRST_RUN, INTERVAL );
+            }, FIRST_RUN, INTERVAL);
         }
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if(Sensor.TYPE_ACCELEROMETER == sensorEvent.sensor.getType()) {
+        if (Sensor.TYPE_ACCELEROMETER == sensorEvent.sensor.getType()) {
             StatisticsBean statisticsBean = new StatisticsBean(sensorEvent.values[0],
                     sensorEvent.values[1], sensorEvent.values[2], System.currentTimeMillis());
             StatisticsManager.getSaver().save(statisticsBean);
@@ -97,4 +98,24 @@ public class AccelerometerService extends Service implements SensorEventListener
             return new StatsCalculator(StatisticsManager.getReader()).steps(new Date(0), new Date());
         }
     };
+
+    public static void setTimeSinceStart(Time timeSinceStart) {
+        AccelerometerService.timeSinceStart = timeSinceStart;
+    }
+
+    public static Date startDate() {
+        Calendar cal = Calendar.getInstance();
+        if(timeSinceStart == null) {
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            return cal.getTime();
+        }
+        Calendar change = Calendar.getInstance();
+        change.setTime(timeSinceStart);
+        cal.set(Calendar.HOUR_OF_DAY, change.get(Calendar.HOUR_OF_DAY));
+        cal.set(Calendar.MINUTE, change.get(Calendar.MINUTE));
+        cal.set(Calendar.SECOND, change.get(Calendar.SECOND));
+        return cal.getTime();
+    }
 }
